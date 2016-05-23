edb5381da795a6cf5fd6ed34a957d9331d87bb9b
Ian Richmond <ian.richmond@ge.com>
2016-05-23 15:08:30 -0400

    new stuff

--- test.py
+++ test.py
@@ -5,7 +5,14 @@ import datetime
 print datetime.datetime.now()
 
 # adding more features
+num = 0
 for x in range(10):
-    x += x
+    num += x
 print x
 print 'some more things happened'
+
+for y in range(20):
+    num += y * y
+
+print y
+print 'final version 2'
